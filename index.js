f7c5aec2b44acd277151638c91c84935ce1d0c2c
Robert D Gauthier <rgauthie@gmail.com>
2018-03-18 01:34:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function total() {
 function removeFromCart(item) {
   for (let i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item){
-      cart.splice(i, 1)
+      cart.splice(i, 1);
       return cart;
     } else {
       if (i === cart.length -  1){
