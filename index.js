301d5f9b3623f4582128ff7e64bc4198cfc94017
Robert D Gauthier <rgauthie@gmail.com>
2018-03-18 01:36:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,8 +48,8 @@ function removeFromCart(item) {
       cart.splice(i, 1);
       return cart;
     } 
-    return `That item is not in your cart.`;
   }
+  return `That item is not in your cart.`;
 }
 
 function placeOrder(cardNumber) {
