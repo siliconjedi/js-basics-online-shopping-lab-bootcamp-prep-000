bd6110c07f64bf6dc83febb2ccbe55e35834e533
Robert D Gauthier <rgauthie@gmail.com>
2018-03-18 00:22:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ function viewCart() {
   } else if (cart.length === 1){
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
   } else {
-    var cartString = "In your cart, you have";
+    var cartString = "In your cart, you have, ";
     for (let i = 0; i < cart.length; i++)
     if (i < cart.length - 1){
-      cartString += `, ${cart[i].itemName} at $${cart[i].itemPrice}`
+      cartString += `${cart[i].itemName} at $${cart[i].itemPrice}`
     } else {
-      cartString += `, and ${cart[i].itemName} at $${cart[i].itemPrice}.`
+      cartString += `and ${cart[i].itemName} at $${cart[i].itemPrice}.`
     }
     return cartString;
   }
